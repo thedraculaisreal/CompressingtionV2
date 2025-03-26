a49ebbd2ee10fa5f849bf3aac6b195f9dc7ae326
thedraculaisreal <jakejackson2546@gmail.com>
2025-03-26 14:56:47 -0400

    byte pair encoddingg

---
+++ build/CMakeFiles/compresser.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compresser.dir/link.d"
+  "CMakeFiles/compresser.dir/src/compression.c.o"
+  "CMakeFiles/compresser.dir/src/compression.c.o.d"
+  "CMakeFiles/compresser.dir/src/file_reading.c.o"
+  "CMakeFiles/compresser.dir/src/file_reading.c.o.d"
+  "CMakeFiles/compresser.dir/src/main.c.o"
+  "CMakeFiles/compresser.dir/src/main.c.o.d"
+  "compresser"
+  "compresser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/compresser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.c
+++ src/main.c
@@ -6,6 +6,17 @@
 #include <errno.h>
 #include "compression.h"
 
+typedef struct {
+    char pair[3];
+    char byte;
+} BytePair;
+
+typedef struct {
+    char pair[3];
+    size_t *index;
+    size_t count;
+} Pair;
+
 static void handle_error(const char *error_string)
 {
     fprintf(stderr, "ERROR: %s", error_string);
@@ -38,13 +49,60 @@ static void read_file(char *file_path, char **buffer)
     fclose(fd);
 }
 
+static int exists(char *pair, Pair** pairs, size_t pair_count)
+{    
+    for (size_t i = 0; i < pair_count; ++i)
+    {
+        if (strcmp(pair, (*pairs)[i].pair) == 0)
+        {
+            return i;
+        }        
+    }
+    return -1;
+}
+
 int main(int argc, char **argv)
 {
     char *buffer;
     read_file(argv[1], &buffer);
+        
+    Pair *pairs = NULL;
     
-    printf("%s", buffer);
+    size_t pair_count = 0;
     
+    for (size_t i = 0; i < strlen(buffer) - 1; ++i)
+    {        
+        char pair[3];
+        pair[2] = '\0';
+        memcpy(pair, &buffer[i], 2);
+        int index = exists(pair, &pairs, pair_count);
+
+        if (index != -1)
+        {            
+            pairs[index].count++;
+            pairs[index].index = (size_t *) realloc(pairs[index].index, sizeof(size_t) * (pairs[index].count + 1));
+            pairs[index].index[pairs[index].count] = i;          
+        }
+        else
+        {            
+            pairs = (Pair *) realloc(pairs, sizeof(Pair) * (pair_count + 1));
+            pairs[pair_count].index = malloc(sizeof(size_t));
+            strcpy(pairs[pair_count].pair, pair);
+            pairs[pair_count].count = 0;
+            pairs[pair_count].index[pairs[pair_count].count] = i;
+            pair_count++;
+        }
+    }
+
+    for (size_t i = 0; i < pair_count; ++i)
+    {        
+        printf("%s -> ", pairs[i].pair);
+        for (size_t j = 0; j < pairs[i].count; ++j)
+        {
+            printf("%zu, ",  pairs[i].index[j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
-
